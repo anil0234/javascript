ee18524de986d28d7b66afc20ea85dfccc84a901
anil0234 <banothanil388@gmail.com>
2025-05-28 08:15:14 +0000

    added javascript basics

--- hello.js
+++ hello.js
@@ -1,16 +1,1 @@
-console.log("Hello world")
-
-var age = 20;
-console.log(age)  // 20
-
-
-var firstName = "Anil";
-console.log(firstName)
-
-firstName = "Madhav"
-
-console.log(firstName)
-
-
-let email = "2@gmail.com";
-email = "3rt"
+hello world
