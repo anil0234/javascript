04fd15f8d558d69a2ba7d694cba42b7145bfc765
anil0234 <banothanil388@gmail.com>
2025-05-28 07:27:48 +0000

    added java script basics

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("Hello world")
+
+var age = 20;
+console.log(age)  // 20
+
+
+var firstName = "Anil";
+console.log(firstName)
+
+firstName = "Madhav"
+
+console.log(firstName)
+
+
+let email = "2@gmail.com";
+email = "3rt"
