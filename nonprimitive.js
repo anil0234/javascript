652f12bcfc7d9a66ff35603f725a5b3164e13d06
anil0234 <banothanil388@gmail.com>
2025-05-30 04:47:31 +0000

    added javascript basics

---
+++ nonprimitive.js
@@ -0,0 +1,19 @@
+// Example
+// Objects
+//Arrays
+
+//Object can store multiple types of data type
+//In objects we store data in key value pair
+
+
+
+let person = {
+    fname:"Anil",
+    age:21,
+    isAlive:true,
+    isSleep:null
+}
+
+console.log(person)
+console.log(person.fname)
+console.log(person.age)
---
+++ objects.js
@@ -0,0 +1,21 @@
+// Read
+// Write 
+// Update
+// Delete
+
+let person ={
+  firstName:"Anil",
+  lastName:"Banoth",
+  email:'banothanil388@gmail.com',   
+}
+
+console.log(person)
+
+// To add a new key in an object
+// phone:"8374128204"
+
+person.phonem="8374128204" // add
+console.log(person)
+
+person.age = 21, //update
+console.log(person)
